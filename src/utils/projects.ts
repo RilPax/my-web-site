4d90cc9acb50c2e73bb6290a8a657c6b0994e781
RilPax <RilPax@yandex.ru>
2025-04-26 22:47:44 +0500

    update-0.98-fix-2

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -8,7 +8,7 @@ type project = {
 
 export const projects: project[] = [
     {
-        src:'/my-site.png',
+        src:'/mysite.png',
         title: 'My Web Site',
         description: "This website serves as my personal portfolio.Here, you'll find a brief overview about me, the skills I have as a developer, and a showcase of the projects I've worked on.",
         link: 'https://normuminovruslan.uz/',
